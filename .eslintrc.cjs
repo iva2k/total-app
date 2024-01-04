9a441e064702f7d68ec341b9ea84955b8dd6cbed
Ilya <iva2k@yahoo.com>
2024-01-03 19:59:58 -0800

    Add imports linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,22 +3,14 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended', // Note: This does not include TS type checks.
+    // 'plugin:@typescript-eslint/recommended-type-checked', // This does type-checking, but breaks in .svelte files and few .ts places
     'plugin:svelte/recommended',
+    'plugin:import/recommended',
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-    extraFileExtensions: ['.svelte']
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
-  },
+  plugins: ['@typescript-eslint', 'import'],
   overrides: [
     {
       files: ['*.svelte'],
@@ -27,5 +19,22 @@ module.exports = {
         parser: '@typescript-eslint/parser'
       }
     }
-  ]
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  },
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.lint.json'],
+    tsconfigRootDir: './',
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  }
 };
