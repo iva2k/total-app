a41daa7b5c6f0e35d28862da4e7c4f4265d382c9
IVA2K <iva2k@yahoo.com>
2024-02-07 09:16:36 -0800

    Add summary to git-merge_main-to-all-ui.sh * Ensure working folder is clean * Refactor code into main()

--- scripts/git-merge_main-to-all-ui.sh
+++ scripts/git-merge_main-to-all-ui.sh
@@ -20,50 +20,121 @@ TARGET_BRANCHES=(
   "ui-tailwindcss"
 )
 
-# Fetch the latest changes from the remote repository
-git fetch origin
+function main() {
+  # Check that local repo is clean
+  if output=$(git status --untracked-files=no --porcelain 2>&1) || [ -z "$output" ]; then
+    # Working directory clean excluding untracked files
+    echo "Working folder is not clean. Please clean working folder and retry."
+    echo "$output"
+    return 1
+  else 
+    echo "Working folder is clean."
+  fi
 
-# Switch to the source branch
-git checkout "$SOURCE_BRANCH"
+  outputs=()
+  errors=()
+  for i in "${!TARGET_BRANCHES[@]}"; do 
+    TARGET_BRANCH="${TARGET_BRANCHES[$i]}"
+    LOGFILE_I="$LOGFILE.$TARGET_BRANCH"
+    (rm "$LOGFILE_I" 2>/dev/null)
+    errors[i]="--"
+    outputs[i]="\033[31m(did not run)\033[36m"
+  done
 
-# Pull the latest changes from the source branch
-git pull origin "$SOURCE_BRANCH"
+  # Fetch the latest changes from the remote repository
+  git fetch origin
 
-# Loop through each target branch and merge changes
-for TARGET_BRANCH in "${TARGET_BRANCHES[@]}"; do
+  # Switch to the source branch
+  git checkout "$SOURCE_BRANCH"
+
+  # Pull the latest changes from the source branch
+  git pull origin "$SOURCE_BRANCH"
+
+  # Loop through each target branch and merge changes
+  for i in "${!TARGET_BRANCHES[@]}"; do 
+    TARGET_BRANCH="${TARGET_BRANCHES[$i]}"
     echo "BEGIN Merging branch \"$SOURCE_BRANCH\" into branch \"$TARGET_BRANCH\"..."
     # Switch to the target branch, Merge changes from the source branch
-    git checkout "$TARGET_BRANCH" && git merge "$SOURCE_BRANCH" --no-edit
+
+    # Assume we will succeed
+    outputs[i]=""
+    errors[i]=0
 
     # Check for merge conflicts
-    if [ $? -ne 0 ]; then
-        echo "Merge conflict detected in \"$TARGET_BRANCH\" branch."
-        echo "  Recreating 'pnpm-lock.yaml' as most likely attempt to resolve conflicts..."
-        (git checkout HEAD -- pnpm-lock.yaml && pnpm install &&  git add pnpm-lock.yaml)
-        echo "  DONE Recreating 'pnpm-lock.yaml'."
-        if git diff --name-only --diff-filter=U | grep -q .; then
-          echo "  There are remaining unresolved Git conflicts:"
-          git diff --name-only --diff-filter=U 
-          echo "  Please resolve any remaining Git conflicts manually, commit and push changes, and run the script again."
-          exit 1
-        else
-          echo "No unresolved Git conflicts found, continuing..."
-          git commit --no-edit
-        fi
-
+    if ! output=$(git checkout "$TARGET_BRANCH") ; then
+      echo "Error checking out branch \"$TARGET_BRANCH\"."
+      outputs[i]="Error checking out branch"
+      errors[i]=1
+      continue
+    fi
+    if ! git merge "$SOURCE_BRANCH" --no-edit ; then
+      echo "Merge conflict detected in \"$TARGET_BRANCH\" branch."
+      echo "  Recreating 'pnpm-lock.yaml' to try resolve most likely conflicts..."
+      (git checkout HEAD -- pnpm-lock.yaml && pnpm install &&  git add pnpm-lock.yaml)
+      echo "  DONE Recreating 'pnpm-lock.yaml'."
+      if git diff --name-only --diff-filter=U | grep -q .; then
+        echo "  There are remaining unresolved Git conflicts in branch \"$TARGET_BRANCH\":"
+        git diff --name-only --diff-filter=U 
+        echo "  Please resolve any remaining Git conflicts manually, commit and push changes, and run this script again."
+        outputs[i]="Merge conflicts, can't resolve automatically"
+        errors[i]=1
+        return 1
+      else
+        echo "  No unresolved Git conflicts found, continuing..."
+        git commit --no-edit
+        outputs[i]="Merge conflicts resolved"
+        errors[i]=0
+      fi
     fi
 
     # Push the changes to the remote repository
     git push origin "$TARGET_BRANCH"
 
-    # # Switch back to the source branch for the next iteration
-    # git checkout "$SOURCE_BRANCH"
-
     echo "DONE Merging branch \"$SOURCE_BRANCH\" into branch \"$TARGET_BRANCH\"."
     echo
-done
+  done
 
-echo "DONE Merging branch \"$SOURCE_BRANCH\" into all target branches."
-echo
+  result=0
+  errors_cnt=0
+  pass_cnt=0
+  total_cnt=0
+  FORMAT="| %-20s | %6s | %-100s |"
+  HEAD=$(printf "$FORMAT" "Branch" "Error" "Output")
+  LINE=$(printf "$FORMAT" "" "")
+  LINE="${LINE// /-}"
+  echo
+  echo "SUMMARY for merge branch \"$SOURCE_BRANCH\" into all target branches:"
+  echo "$LINE"
+  echo "$HEAD"
+  echo "$LINE"
+  for i in "${!TARGET_BRANCHES[@]}"; do 
+    TARGET_BRANCH="${TARGET_BRANCHES[$i]}"
+    error="${errors[$i]}"
+    output=$(decolor "${outputs[$i]}")
+    total_cnt=$((total_cnt+1))
+    color_red=0
+    if [ "$error" == "0" ]; then
+      pass_cnt=$((pass_cnt+1))
+    elif [ "$error" != "--" ]; then
+      result=1
+      color_red=1
+      errors_cnt=$((errors_cnt+1))
+    fi
+    [ "$color_red" -ne 0 ] && echo -n -e "\033[31m"
+    printf "$FORMAT\n" "$TARGET_BRANCH" "$error" "${output:0:100}"
+    [ "$color_red" -ne 0 ] && echo -n -e "\033[36m"
+  done
+  echo "$LINE"
+  printf "$FORMAT\n" "Total:" "$errors_cnt" ""
+  echo "$LINE"
+  echo
 
-git checkout "$SOURCE_BRANCH"
+  echo "DONE Merging branch \"$SOURCE_BRANCH\" into $total_cnt target branches, $pass_cnt passed, $errors_cnt error(s)."
+  echo
+
+  git checkout "$SOURCE_BRANCH"
+
+  return $result
+}
+
+main "@$"
