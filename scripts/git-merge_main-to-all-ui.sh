ee9e6036f0d888f7b3422e266d7d8d4e5c0ffe5b
IVA2K <iva2k@yahoo.com>
2024-02-12 22:07:14 -0800

    Improve scripts/git-merge_main-to-all-ui.sh * Check local is in sync with remote * update non-conflicting lockfile only if package.json was changed

--- scripts/git-merge_main-to-all-ui.sh
+++ scripts/git-merge_main-to-all-ui.sh
@@ -52,6 +52,15 @@ function merge_to_all() {
     echo "Working folder is clean."
   fi
 
+  if ! output=$(git rev-list "${SOURCE_BRANCH}...origin/${SOURCE_BRANCH}" 2>&1) || [ -n "$output" ]; then
+    # Branch is different from origin
+    echo "Branch \"$SOURCE_BRANCH\" in local repo is not synced with remote origin. Please sync local branch and retry."
+    echo "$output"
+    return 1
+  else 
+    echo "Local branch is in sync."
+  fi
+
   outputs=()
   errors=()
   for i in "${!TARGET_BRANCHES[@]}"; do 
@@ -89,8 +98,14 @@ function merge_to_all() {
       continue
     fi
 
-    # Merge and check for conflicts
-    if ! git merge "$SOURCE_BRANCH" --no-edit ; then
+    # Merge but not commit
+    # if ! git merge "$SOURCE_BRANCH" --no-edit ; then
+    git merge "$SOURCE_BRANCH" --no-commit
+    res=$?
+    # Check if package.json file is updated (or conflicted)
+    (git diff --exit-code "package.json" >/dev/null 2>&1) && pkg_updated=0 || pkg_updated=1
+
+    if [ "$res" -ne 0 ] ; then
       echo "Merge conflict detected in \"$TARGET_BRANCH\" branch."
 
       # Check for conflicts in package.json (which will break rebuilding pnpm-lock.yaml)
@@ -102,10 +117,10 @@ function merge_to_all() {
         return 1
       fi
 
-      echo "  Recreating 'pnpm-lock.yaml' to try resolve most likely conflicts..."
+      echo "  Recreating \"pnpm-lock.yaml\" to try resolve most likely conflicts..."
       ( "${COMMAND_MERGE_LOCKFILE[@]}")
       error=$?
-      echo "  DONE Recreating 'pnpm-lock.yaml', error=$error."
+      echo "  DONE Recreating \"pnpm-lock.yaml\", error=$error."
 
       if git diff --name-only --diff-filter=U | grep -q .; then
         echo "  There are remaining unresolved Git conflicts in branch \"$TARGET_BRANCH\":"
@@ -120,12 +135,18 @@ function merge_to_all() {
         outputs[i]="Merge conflicts resolved"
         errors[i]=0
       fi
+    else
+      git commit --no-edit
     fi
 
-    echo "  Updating 'pnpm-lock.yaml'..."
-    ( "${COMMAND_UPDATE_LOCKFILE[@]}" )
-    error=$?
-    echo "  DONE Updating 'pnpm-lock.yaml', error=$error."
+    if [ "$pkg_updated" -ne 0 ] ; then
+      echo "  Changes to \"package.json\" file were merged, updating \"pnpm-lock.yaml\"..."
+      outputs[i]="${outputs[i]}, package.json merged and lockfile updated"
+
+      ( "${COMMAND_UPDATE_LOCKFILE[@]}" )
+      error=$?
+      echo "  DONE Updating \"pnpm-lock.yaml\", error=$error."
+    fi
 
     # Push the changes to the remote repository
     git push origin "$TARGET_BRANCH"
