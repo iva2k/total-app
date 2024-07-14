0b783d525c7d6865e59cfeb1bc1b15a239e8c7f9
IVA2K <iva2k@yahoo.com>
2024-07-14 12:19:56 -0700

    Sanitize $page.url.pathname in Header.svelte

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
   import { page } from '$app/stores';
   import PureHeader from './PureHeader.svelte';
+  $: pathname = browser ? ($page.url.pathname ?? '') : '';
 </script>
 
 <div>
-  <!-- TODO: (when fixed) Remove eslint-disable -->
-  <!-- eslint-disable-next-line svelte/valid-compile -->
-  <PureHeader pathname={$page.url.pathname}><slot /></PureHeader>
+  <PureHeader {pathname}><slot /></PureHeader>
 </div>
