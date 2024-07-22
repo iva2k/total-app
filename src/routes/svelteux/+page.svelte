e61028532d3c98525661956c91cf67ab538391c1
IVA2K <iva2k@yahoo.com>
2024-07-22 15:38:10 -0700

    Add clickCounter to the demo button

--- src/routes/svelteux/+page.svelte
+++ src/routes/svelteux/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
   import { Button } from 'svelte-ux';
+
+  let clickCounter = $state(0);
+  function onClick(e: MouseEvent) {
+    clickCounter += 1;
+  }
 </script>
 
-<p>Here comes the button:</p>
-<Button>Click Here</Button>
+<div>
+  <p>Here comes the button:</p>
+  <Button onclick={onClick}>Click Here</Button>
+  {#if clickCounter > 0}
+    <p>Clicked {clickCounter} times.</p>
+  {/if}
+</div>
