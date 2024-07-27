644ce0a21ec944da611d6b627f7cc919e9840037
IVA2K <iva2k@yahoo.com>
2024-07-27 07:20:35 -0700

    Change postcss.config.cjs to array plugins (use `require(...)`) * Add type to postcss.config.cjs * Add comments to postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,11 @@
-const autoprefixer = require('autoprefixer');
-
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-nesting': {},
-    autoprefixer: {}
-  }
+  plugins: [
+    require('postcss-import'), // Process @import
+    require('postcss-nesting'), // For CSS Nesting Specification https://www.w3.org/TR/css-nesting-1/
+    require('autoprefixer')
+    //  require('postcss-nested'), // For sass syntax nesting
+  ]
 };
 
 module.exports = config;
