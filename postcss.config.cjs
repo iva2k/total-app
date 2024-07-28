f08468ea797334377aee656329805bfa7242ee4a
IVA2K <iva2k@yahoo.com>
2024-07-28 16:52:36 -0700

    Fix merge conflict in postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,20 +1,15 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-<<<<<<< HEAD
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-=======
   plugins: [
     require('postcss-import'), // Process @import
-    require('postcss-nesting'), // For CSS Nesting Specification https://www.w3.org/TR/css-nesting-1/
+
+    // Choose one:
+    // require('tailwindcss/nesting'), // Use 'postcss-nested' by default for sass syntax nesting
+    require('tailwindcss/nesting')('postcss-nesting'), // 'postcss-nesting' for CSS Nesting Specification https://www.w3.org/TR/css-nesting-1/
+
+    require('tailwindcss'), // order is important!
     require('autoprefixer')
-    //  require('postcss-nested'), // For sass syntax nesting
   ]
->>>>>>> main
 };
 
 module.exports = config;
