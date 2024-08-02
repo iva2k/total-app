7e19e87b4b0bbf98a1c1449c5b1d29458dee9587
IVA2K <iva2k@yahoo.com>
2024-08-02 09:37:33 -0700

    Deconfuse flowbite +page.svelte message

--- src/routes/flowbite/+page.svelte
+++ src/routes/flowbite/+page.svelte
@@ -5,6 +5,6 @@
 <div class="p-8">
   <Alert>
     <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
+    Flowbite UI is loaded. Change a few things up and try submitting again.
   </Alert>
 </div>
