00bd18059551cc971b33361498e0748353d95272
IVA2K <iva2k@yahoo.com>
2024-08-04 10:45:34 -0700

    Disable svelte default SW (so virtual:pwa-register/svelte can be the only one)

--- svelte.config.js
+++ svelte.config.js
@@ -65,6 +65,9 @@ const config = {
         pwaConfiguration.srcDir || 'src',
         pwaConfiguration.filename || 'sw.js'
       )
+    },
+    serviceWorker: {
+      register: false // Disable SvelteKit service worker to use virtual:pwa-register/svelte
     }
   },
 
